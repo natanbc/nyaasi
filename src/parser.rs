eb1a8129d9a98a4214e9fe98df3229c8bfe8e44b
natanbc <natanbc@users.noreply.github.com>
2019-09-14 20:33:22 -0300

    properly parse urls now

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,7 @@
 use kuchiki::traits::*;
 use kuchiki::{ElementData, NodeRef, NodeDataRef};
 use serde_derive::Serialize;
+use url::Url;
 
 use super::magnet_uri;
 
@@ -49,7 +50,8 @@ impl Results {
     }
 }
 
-pub fn parse(html: &str, current_url: &str) -> Option<Results> {
+pub fn parse(html: &str, url: &str) -> Option<Results> {
+    let current_url = Url::parse(url).ok()?;
     let dom = kuchiki::parse_html().one(html);
 
     let table = dom.select_first("tr.success > td.text-center > a > i.fa-magnet")
@@ -63,13 +65,13 @@ pub fn parse(html: &str, current_url: &str) -> Option<Results> {
     let mut entries = table.select("tr.success").ok()?.map(|row| {
         use std::str::FromStr;
 
-        let magnet_uri = select_parent_href(row.as_node(), "td.text-center:nth-child(3) > a > i.fa-magnet", current_url)?;
+        let magnet_uri = select_parent_href(row.as_node(), "td.text-center:nth-child(3) > a > i.fa-magnet", &current_url)?;
         let magnet = magnet_uri::MagnetURI::from_str(&magnet_uri).ok();
 
         Some(AnimeEntry {
             name: select_text(row.as_node(), "td:nth-child(2) > a:not(.comments)")?,
             links: Links {
-                torrent: select_parent_href(row.as_node(), "td.text-center:nth-child(3) > a > i.fa-download", current_url)?,
+                torrent: select_parent_href(row.as_node(), "td.text-center:nth-child(3) > a > i.fa-download", &current_url)?,
                 magnet: magnet_uri
             },
             size: select_text(row.as_node(), "td.text-center:nth-child(4)")?,
@@ -82,12 +84,12 @@ pub fn parse(html: &str, current_url: &str) -> Option<Results> {
     }).collect::<Option<Vec<_>>>()?;
 
     let pagination = dom.select_first("ul.pagination > li.active > a").ok()
-        .and_then(|e| make_page(&e, current_url))
+        .and_then(|e| make_page(&e, &current_url))
         .and_then(|current| {
             Some(Pagination {
                 pages: dom.select("ul.pagination > li:not(.disabled) > a:not([rel])").ok()?
                     .map(|e| {
-                        make_page(&e, current_url)
+                        make_page(&e, &current_url)
                     }).collect::<Option<Vec<_>>>()?,
                 current: current
             })
@@ -103,7 +105,7 @@ pub fn parse(html: &str, current_url: &str) -> Option<Results> {
 }
 
 #[inline]
-fn make_page(e: &NodeDataRef<ElementData>, current_url: &str) -> Option<Page> {
+fn make_page(e: &NodeDataRef<ElementData>, current_url: &Url) -> Option<Page> {
     Some(Page {
         url: href(e.as_node(), current_url)?,
         number: e.text_contents().split_whitespace().next()
@@ -127,20 +129,15 @@ fn select_u32(node: &NodeRef, sel: &str) -> Option<u32> {
 }
 
 #[inline]
-fn select_parent_href(a: &NodeRef, sel: &str, current_url: &str) -> Option<String> {
+fn select_parent_href(a: &NodeRef, sel: &str, current_url: &Url) -> Option<String> {
     href(&select_parent(a, sel)?, current_url)
 }
 
 #[inline]
-fn href(a: &NodeRef, current_url: &str) -> Option<String> {
-    a.as_element()?.attributes.borrow().get("href").map(|e| e.to_owned()).map(|url| {
-        if url.starts_with("//") {
-            format!("https:{}", url)
-        } else if url.starts_with("/") {
-            format!("https://nyaa.si{}", url)
-        } else {
-            format!("{}{}", current_url, url)
-        }
-    })
+fn href(a: &NodeRef, current_url: &Url) -> Option<String> {
+    a.as_element()?.attributes.borrow().get("href")
+        .and_then(|url| {
+            current_url.join(url).ok().map(|u| u.into_string())
+        })
 }
 
